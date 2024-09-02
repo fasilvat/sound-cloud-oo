b6c4ae7ea23e85f12d226fc12fa5f1c58339ba8f
Felipe Silva <13461652+fasilvat@users.noreply.github.com>
2024-09-02 21:19:18 +0000

    Remove teste antigo

--- SoundCloudOO/Program.cs
+++ SoundCloudOO/Program.cs
@@ -3,8 +3,3 @@ musica1.Nome = "Só Os Loucos Sabem";
 musica1.Artista = "Charlie Brown Jr.";
 musica1.ExibirNomeEArtista();
 Console.WriteLine(musica1.DescricaoResumida);
-
-Teste teste = new Teste();
-teste.MostrarPrimos();
-
-List<int> numeros = new List<int>();
