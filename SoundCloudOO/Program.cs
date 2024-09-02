4cf45c56f2c09e83c1488be46d101d2f675501fd
Felipe Silva <13461652+fasilvat@users.noreply.github.com>
2024-09-02 21:22:47 +0000

    Cria álbum e músicas, e adiciona músicas ao álbum

--- SoundCloudOO/Program.cs
+++ SoundCloudOO/Program.cs
@@ -1,5 +1,11 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Só Os Loucos Sabem";
-musica1.Artista = "Charlie Brown Jr.";
-musica1.ExibirNomeEArtista();
-Console.WriteLine(musica1.DescricaoResumida);
+﻿Album album = new Album();
+album.Nome = "Imunidade Musical";
+
+Musica musica1 = new Musica();
+musica1.Nome = "No passo a passo";
+
+Musica musica2 = new Musica();
+musica2.Nome = "É quente";
+
+album.AdicionarMusica(musica1);
+album.AdicionarMusica(musica2);
